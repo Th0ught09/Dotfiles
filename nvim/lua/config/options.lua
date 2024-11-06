df12d598a28a67e9ebef343b97c840d80a8a5028
Th0ught09 <kirkmatthew440@gmail.com>
2024-11-06 08:10:05 +0000

    nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,4 +4,22 @@
 vim.opt.hidden = true
 vim.o.grepprg = "rg --vimgrep --smart-case --follow"
 vim.g.lazyvim_picker = "fzf"
-vim.opt.clipboard = unnamed
+vim.o.scrolloff = 8
+-- vim.o.clipboard = "unnamedplus"
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.foldmethod = "manual"
+-- vim.o.nnoremap = "<C-w> "
+vim.g.clipboard = {
+  name = "xclip",
+  copy = {
+    ['+'] = 'xclip -selection clipboard',
+    ['*'] = 'xclip -selection primary',
+  },
+  paste = {
+    ['+'] = 'xclip -selection clipboard -o',
+    ['*'] = 'xclip -selection primary -o',
+  },
+  cache_enabled = 0,
+}
