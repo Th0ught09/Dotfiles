e10db7a17a97739d72ebb6b29ac5e9a905514617
Kirk Matt <kirkmatt@proton.me>
2025-03-26 08:12:14 +0000

    usual push

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,8 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/Documents/Org/rules.org" "/home/kirkm/Documents/Org/jv_time.org"
-     "/home/kirkm/Documents/Org/sa.org"
+   '("~/Documents/Org/tips.org" "/home/kirkm/Documents/Org/rules.org"
+     "/home/kirkm/Documents/Org/jv_time.org" "/home/kirkm/Documents/Org/sa.org"
      "/home/kirkm/Documents/Org/data_dependecies.org"
      "/home/kirkm/Documents/Org/gp_cli.org"
      "/home/kirkm/Documents/Org/tasks.org")))
